37de48214ebf91a7697a736f381a60c4eba62753
t-perniola <t.perniola1@studenti.uniba.it>
2024-05-27 18:12:01 +0200

    updated SMT model but still not working
    Issues:
    1) channeling seems like it doesn't work
    2) same distances for each courier
    3) two mandatory constraints make the model unsat

--- project/CPProject/SMT_model.py
+++ project/CPProject/SMT_model.py
@@ -1,12 +1,18 @@
 from z3 import *
 
+# THEORIES USED
+# - Boolean Logic
+# - Arithmetic
+# - EUF (uninterpreted functions)
+
 # TODO dynamic data initializations
 m = 2
 n = 3
 
+max_nodes = (n // m) + 3
+
 COURIERS = range(m)
-ITEMS = range(n)
-TIMESTEPS = range(n+2) # including starting & ending times (that would still coincide in the origin point)
+NODES = range(n)
 
 l = [18, 30]
 s = [20, 17, 6]
@@ -18,168 +24,156 @@ D = [[0, 21, 86, 99],
 
 # VARIABLES
 # creates m x n variables that tell if the item j is assigned to courier i
-x = [[Bool(f"x_{c}_{i}") for i in ITEMS] for c in COURIERS]
-print(x)
+b_path = [[Bool(f"x_{c}_{i}") for i in NODES] for c in COURIERS]
 '''
 Example of the matrix 'x':
 x = [1, 0, 0, 1]  # Courier 1: items 1 and 4 are assigned (x_1_1, x_1_4 = True)
-    [0, 1, 0, 0]  # Courier 2: items 2 is assigned
+    [0, 1, 0, 0]  # Courier 2: item 2 is assigned
     [0, 0, 1, 0]  # Courier 3: item 3 is assigned
 '''
 
 # load of each item picked up by each courier
 # - I have to sum only the variables assigned to True, namely only when x_ij = 1
-load = [Sum([If(x[c][i], s[i], 0) for i in ITEMS]) for c in COURIERS]
-print(load)
+load = [Sum([If(b_path[c][i], s[i], 0) for i in NODES]) for c in COURIERS]
 
-# tour matrix: defines the order of the distribution points (where items are) visited
-# path[c][t] indicates the item visited by courier c at timestep t
-path = [[Int(f"p_{c}_{t}") for t in TIMESTEPS] for c in COURIERS]
-print("EMPTY PATH: ", path)
+# tour matrix: defines the order of the distribution points (where NODES are) visited
+path = [[Int(f"p_{c}_{j}") for j in range(max_nodes)] for c in COURIERS]
+print("Empty path: ", path)
 '''
 Example of the matrix 'path' (@ stands for the origin point):
-path = [@, 3, 0, 0, 0, @]  # Courier 1 path:  origin --> 3 --------> origin
-       [@, 2, 0, 0, 0, @]  # Courier 2 path:  origin --> 2 --------> origin
-       [@, 1, 4, 0, 0, @]  # Courier 3 path:  origin --> 1 --> 4 --> origin
+path = [5, 3, 5]     # Courier 1 path:  origin --> 3 --------> origin
+       [5, 2, 5]     # Courier 2 path:  origin --> 2 --------> origin
+       [5, 1, 4, 5]  # Courier 3 path:  origin --> 1 --> 4 --> origin
 '''
 
 # max distance for each courier
-max_distance = Int("max_dist")
+total_distance = [Int(f"max_dist_{c}") for c in COURIERS]
 
 # Define variables for the distance traveled by each courier
 dist = [Int(f'dist_{c}') for c in COURIERS]
 
-# SOLVER INIT
+# path length for each courier
+path_length = [Int(f"len_{c}") for c in COURIERS]
+
+# HELPER functions
+# - all_different
+def distinct_except(values, forbidden_values):
+    non_forbidden_values = [v for v in values if v not in forbidden_values]
+    return Distinct(non_forbidden_values) # enforcing uniqueness
+
+# - among
+def among(value_set, value):
+    return Or([value == v for v in value_set])
+
+# OPTIMIZING SOLVER INIT
 solver = Solver()
 
 # CONSTRAINTS
-# each courier should not overload itself, namely it mustn't exceed its load capacity
+# path should range between 0 and n+1
 for c in COURIERS:
-    solver.add(load[c] <= l[c]) # remember: solver.add('expression') the solution must satisfy the given expression
+    for j in range(max_nodes):
+        solver.add(And(path[c][j] >= 0, path[c][j] <= n+1))
 
-# each item should be assigned to exactly one courier, namely each item can be picked only once
-# - what about using instead (Exactly <-> AtLeast and AtMost) ?
-for i in ITEMS:
-    solver.add(Sum([If(x[c][i], 1, 0) for c in COURIERS]) == 1)
+# path length should range between 3 and max_nodes
+for c in COURIERS:
+    solver.add(And(path_length[c] >= 3, path_length[c] <= max_nodes))
 
-# the boundaries (starting and ending point) must be equal to n+1
+# Couriers cannot visit same node twice (nor stay in the same node)
+for c in COURIERS:
+    solver.add(distinct_except([path[c][j] for j in range(1, max_nodes)], [0]))
+
+# Define initial node and final node
 for c in COURIERS:
     solver.add(path[c][0] == n+1)
-    solver.add(path[c][n+1] == n+1)
+    #solver.add(path[c][path_length[c]] == n + 1)
 
-# each courier should pick at least one item [redundant: it can be inferred from other constraints]
+# Set to zero unvisited nodes
 for c in COURIERS:
-    #solver.add(Or([x[c][i]] for i in ITEMS)) # iterating along the rows of x
-    solver.add(Sum([x[c][i] for i in ITEMS]) >= 1)
+    for j in range(max_nodes):
+        solver.add(If(j > path_length[c], path[c][j] == 0, True))
 
-# all items should be picked-up
-for i in ITEMS:
-    #solver.add(Or([x[c][i]] for c in COURIERS)) # iterating along the columns of x
-    solver.add(Sum([x[c][i] for c in COURIERS]) >= 1)
+# If you have more load size than me, then your load must be greater than mine
+for c1 in COURIERS:
+    for c2 in COURIERS:
+        print()
+        solver.add(Sum([If(And(c2 > c1, l[c2] > l[c1]), b_path[c1][j]*s[j], 0) for j in NODES])
+                <= Sum([b_path[c2][j]*s[j] for j in NODES]))
 
-# a courier cannot pick all the items, moreover it can pick at most (n-m+1) items [redundant wrt the objective function]
-for c in COURIERS:
-    solver.add(Sum([If(x[c][i], 1, 0) for i in ITEMS]) <= n-m+1)
-
-# Channeling: If an item appears in any path in the 'tour' matrix, then it must be True in the 'x' matrix (and viceversa)
-for c in COURIERS:
-    for i in ITEMS: 
-        solver.add(And(
-            Implies(Or([path[c][t] == i for t in range(1, n+1)]), x[c][i] == True),
-            Implies(x[c][i] == True, Or([path[c][t] == i for t in range(1, n+1)]))))
-            # no built-in for iff (<->) then use let's use the construct: And(Implies(X), Implies(Y))
-
-# no zeros between two numbers
-#for c in COURIERS:
-#    solver.add([(path[c][t], path[c][t+1]) for t in range(1, n+1)])
-
-# distances computation TODO
-# 1st sum) from origin to the distribution point where x[c][i] is true, namely if the courier c has picked-up the i-th item
-# 2nd sum) from each distribution point to the following one
-# 3rd sum) from the last distribution point back to the origin
+# All the couriers must visit different nodes
 '''
-for c in COURIERS: 
-    print([path[c][t] for t in TIMESTEPS])
-    print(path[c][1])
-    distances = D[n][path[c][1]]
-    print("1) ", distances)
-
-    for t in range(2, n+1):
-        distances += If(And(path[c][t] != 0, path[c][t+1]!=0, True), D[path[c][t]][path[c][t+1]], 0)
-    print("2) ", distances)
-    
-    for t in reversed(range(2, n+1)):
-        if path[c][t] != 0:
-            last_item = path[c][t]
-        break  # Exit the loop when the first non-zero value is found
-    print("last ", last_item)
-
-    distances += D[last_item][n]
-    print("3) ", distances)
+for c in COURIERS:
+    solver.add(distinct_except([path[c][j] for j in range(max_nodes)], [0, n+1]))
 '''
 
-        
-# minimize max distance among all couriers
-#for c in COURIERS:
-#    solver.add(dist[c] == distances)
+for i in NODES:
+    solver.add(Sum([If(b_path[c][i] == True, 1 , 0) for c in COURIERS]) == 1)
 
+for j in NODES:
+    solver.add(Or([b_path[c][j] == True for c in COURIERS]))
+
+# The path length of the single courier, in order to be balanced, cannot be longer than n-m
+for c in COURIERS:
+    solver.add(path_length[c] <= max_nodes)
+
+for c in COURIERS:
+    solver.add(Sum([If(b_path[c][i] == True, 1, 0) for i in NODES]) <= max_nodes)
+
+# CHECK SATISFIABILITY
 if solver.check() == sat:
-    print("\nThe model is satisfiable:")
-
-    # a model represents an assignment of values to variables that satisfies all the constraints provided to the solver
     model = solver.model()
 
-    # Print the path matrix
-    path_values = [[model[path[c][t]] for t in TIMESTEPS] for c in COURIERS]
-    print("\nPaths:")
-    for c in COURIERS:
-        print(f"Courier {c}: {path_values[c]}")
+ ############################################################################à
 
-    # Print the x matrix with True/False values
-    print("\nItem Assignments (True/False):")
+    # ADDITIONAL CONSTRAINTS unfeasible before
+    # Define final node
     for c in COURIERS:
-        print(f"Courier {c}: {[model.eval(x[c][i]) for i in ITEMS]}")
+        path_length_value = model.eval(path_length[c])
+        solver.add(path[c][path_length_value.as_long()] == n + 1)
+
+    # Channeling constraint
+    for c in COURIERS:
+        path_length_value = model.eval(path_length[c])
+        for i in NODES:
+            solver.add(Implies(b_path[c][i], Or([path[c][j] == i for j in range(1, path_length_value.as_long() + 1)])))
+            solver.add(Implies(Or([path[c][j] == i for j in range(1, path_length_value.as_long() + 1)]), b_path[c][i]))
+
+    # Distance computation
+    for c in COURIERS:
+        path_length_value = model.eval(path_length[c]).as_long()
+        total_distance_expr = Sum([D[model.eval(path[c][j]).as_long()-1][model.eval(path[c][j+1]).as_long()-1] for j in range(path_length_value)])
+        solver.add(total_distance[c] == total_distance_expr)
+
+    # All the adjacent values until path_length must be different from 0
+    for c in COURIERS:
+        path_length_value = model.eval(path_length[c])
+        for j in range(1, path_length_value.as_long()):
+            solver.add(And(model[path[c][j]] != 0, model[path[c][j+1]] != 0))
+
+    '''
+    # Load size constraint
+    for c in COURIERS:
+        path_length_value = model.eval(path_length[c])
+        solver.add(Sum([s[model[path[c][j]].as_long()] for j in range(1, path_length_value.as_long())]) <= l[c])
+    '''
+    # Rerun the solver
+    if solver.check() == sat:
+        model = solver.model()
+
+        path_values = [[model[path[c][j]] for j in range(max_nodes)] for c in COURIERS]
+        print("\nPaths:")
+        for c in COURIERS:
+            print(f"Courier {c}: {path_values[c]}")
+
+        print("\nItem Assignments (T/F):")
+        for c in COURIERS:
+            print(f"Courier {c}: {[model.eval(b_path[c][i]) for i in NODES]}")
+
+        print("\nTotal distances:")
+        for c in COURIERS:
+            print(f"Courier {c}: {model.eval(total_distance[c])}")
+
+    else:
+        print("The model is unsatisfiable after adding new constraints.")
+
 else:
     print("The model is unsatisfiable.")
-
-# OBJECTIVE: minimize maximum distance
-
-#This approach effectively transforms the optimization problem into a series of decision problems,
-#leveraging the SAT solver to find the optimal solution, until the smallest feasible maximum distance is found. 
-# Function to check feasibility with a given max distance
-def is_feasible(max_dist):
-    temp_solver = Solver()
-    temp_solver.add(solver.assertions())
-    for c in COURIERS:
-        temp_solver.add(dist[c] <= max_dist)
-    if temp_solver.check() == sat:
-        return True, temp_solver.model()
-    return False, None
-
-'''
-# Binary search to minimize the maximum distance
-lower_bound = 0
-upper_bound = sum(max(D[i]) for i in range(n + 1))  # Upper bound estimate
-optimal_distance = upper_bound
-optimal_model = None
-
-while lower_bound <= upper_bound:
-    mid = (lower_bound + upper_bound) // 2
-    feasible, model = is_feasible(mid)
-    if feasible:
-        optimal_distance = mid
-        optimal_model = model
-        upper_bound = mid - 1
-    else:
-        lower_bound = mid + 1
-
-# Display the optimal solution
-if optimal_model:
-    path_solution = [[optimal_model[path[c][t]].as_long() if optimal_model[path[c][t]] else n for t in TIMESTEPS] for c in COURIERS]
-    print("Optimal max distance:", optimal_distance)
-    print("Paths:", path_solution)
-    for c in COURIERS:
-        print(f"Courier {c} path: ", [optimal_model[path[c][t]] for t in TIMESTEPS])
-else:
-    print("No solution found")
-'''
