3f7671012299355cafb3178ebbac10cc81501398
t-perniola <t.perniola1@studenti.uniba.it>
2024-06-02 14:33:02 +0200

    improved notation and added paths plotting

--- project/SMT/SMT_model.py
+++ project/SMT/SMT_model.py
@@ -1,8 +1,10 @@
 from z3 import *
 from utils import *
+import matplotlib.pyplot as plt
+import networkx as nx
 import time
 
-# THEORIES USED
+# NOTE: THEORIES USED
 # - Boolean Logic
 # - Arithmetic
 # - EUF (uninterpreted functions)
@@ -15,35 +17,36 @@ start_time = time.time()
 # Directory containing .dat files
 directory = 'instances'
 
+# Choose the instance
+NUM_INST = 2
+
 # Read all .dat files and populate instances
-num_inst = 5 # choose the instance
 instances = read_all_dat_files(directory)
-instance = instances[num_inst-1] # extract the corresp. instance
+instance = instances[NUM_INST-1] # extract the corresp. instance
 m = instance['m']
 n = instance['n']
 l = instance['l']
 s = instance['s']
 D = instance['D']
 
-# useful variables
-max_nodes = (n // m) + 3
-COURIERS = range(1, m+1)
-NODES = range(1, n+1)
-#print("max_nodes:", max_nodes)
-print(f"Instance {num_inst} chosen \nnum couriers: {m}, num items: {n}")
+# DECLARING CONSTANTS
+MAX_ITEMS = (n // m) + 3
+Couriers = range(1, m+1)
+Items = range(1, n+1)
+print(f"Instance {NUM_INST} chosen \nnum Couriers: {m}, num items: {n}")
 
 # DECLARING VARIABLES USING SMT SORTS
-# creates m x n variables that tell if the item j is assigned to courier i
 Z = IntSort()
-b_path = Array("b_path", IntSort(), ArraySort(IntSort(), BoolSort()))
+B = BoolSort()
+b_path = Array("b_path", Z, ArraySort(Z, B)) # create a boolean matrix
 load = Array('load', Z, Z)
 size = Array('size', Z, Z)
-path = Array('path', Z, ArraySort(Z, Z))
+path = Array('path', Z, ArraySort(Z, Z)) # create an integer matrix
 path_length = Array('path', Z, Z)
 total_distance = Array('total_distance', Z, Z)
 D_func = Function('D_func', Z, Z, Z) # takes two integer arguments (indices) and returns an integer (distance)
 
-# OPTIMIZING optimizer INIT
+# INITIALIZE the OPTIMIZER
 optimizer = Optimize()
 
 # HELPER FUNCTIONS
@@ -63,139 +66,154 @@ def lexleq(a1, a2):
 
 # CONSTRAINTS
 # Mappings (due to 0-indexing): Z3 Arrays <-> Python arrays
+# - distance matrix
 for i in range(n+1):
     for j in range(n+1):
         optimizer.add(D_func(i, j) == D[i][j])
-
+# items' sizes
 for i in range(n):
     optimizer.add(size[i+1] == s[i])
-
+# Couriers' laod capacities
 for c in range(m):
     optimizer.add(load[c+1] == l[c])
 
 # Path should range between 0 and n+1        
-for c in COURIERS:
-    for j in range(1, max_nodes+1):
+for c in Couriers:
+    for j in range(1, MAX_ITEMS+1):
         optimizer.add(And(path[c][j] >= 0, path[c][j] <= n+1))
 
 # Boundaries of path_length's values
-for c in COURIERS:
-    optimizer.add(And(path_length[c] >= 3, path_length[c] <= max_nodes))
+for c in Couriers:
+    optimizer.add(And(path_length[c] >= 3, path_length[c] <= MAX_ITEMS))
 
 # Define initial node and final node
-for c in COURIERS:
+for c in Couriers:
     optimizer.add(path[c][1] == n + 1) # Initial node
     optimizer.add(path[c][path_length[c]] == n + 1)  # Ending node
 
-# Set unvisited nodes to zero
-for c in COURIERS:
-    for i in range(1, max_nodes + 1):
+# Set unvisited Items to zero
+for c in Couriers:
+    for i in range(1, MAX_ITEMS + 1):
         optimizer.add(Implies(i > path_length[c], path[c][i] == 0))
 
 # No courier exceeds its load capacity
-for c in COURIERS:    
-    load_expr = Sum([If(b_path[c][j], size[j], 0) for j in NODES])
+for c in Couriers:    
+    load_expr = Sum([If(b_path[c][j], size[j], 0) for j in Items])
     optimizer.add(load_expr <= load[c])
 
 # Exactly one item assignment to a courier
-for j in NODES:
-    optimizer.add(Sum([If(b_path[c][j], 1, 0) for c in COURIERS]) == 1)
+for j in Items:
+    optimizer.add(Sum([If(b_path[c][j], 1, 0) for c in Couriers]) == 1)
 
-# A courier cannot take more than max_nodes items
-for c in COURIERS:
-    optimizer.add(Sum([If(b_path[c][j], 1, 0) for j in NODES]) <= max_nodes)
+# A courier cannot take more than MAX_ITEMS items
+for c in Couriers:
+    optimizer.add(Sum([If(b_path[c][j], 1, 0) for j in Items]) <= MAX_ITEMS)
 
 # Couriers cannot visit same node twice (nor stay in the same node)
-for c in COURIERS:
-    optimizer.add(distinct_except([path[c][j] for j in range(1, max_nodes)], [0]))
+for c in Couriers:
+    optimizer.add(distinct_except([path[c][j] for j in range(1, MAX_ITEMS)], [0]))
 
 '''
-# All the nodes must be visited at least once (redundant)
-for i in NODES:
-    optimizer.add(Or([b_path[c][j] for c in COURIERS]))
+# No zeros in each path: (FIXME: it does not work, therefore we allow them to be present)
+for c in Couriers:
+    for i in range(2, MAX_ITEMS):
+        optimizer.add(Implies(i < path_length[c]-1, path[c][i] != 0))
+        optimizer.add(Implies(i < path_length[c]-1, path[c][i+1] != 0))
 '''
-    
+
+'''
+# All the Items must be visited at least once (NOTE: redundant)
+for i in Items:
+    optimizer.add(Or([b_path[c][j] for c in Couriers]))
+'''
+
 # Channeling: 
 # - b_path -> path
-for c in COURIERS:
-    for i in NODES: # if b_bath[c][i] is true, then there must be true also at least one path[c][j] == i
-        optimizer.add(Implies(b_path[c][i], Or([path[c][j] == i for j in range(1, max_nodes+1)])))
+for c in Couriers:
+    for i in Items: # if b_bath[c][i] is true, then there must be true also at least one path[c][j] == i
+        optimizer.add(Implies(b_path[c][i], Or([path[c][j] == i for j in range(1, MAX_ITEMS+1)])))
         # if b_bath[c][i] is false, then there won't be any path[c][j] == i
-        optimizer.add(Implies(Not(b_path[c][i]), And([path[c][j] != i for j in range(1, max_nodes+1)])))
+        optimizer.add(Implies(Not(b_path[c][i]), And([path[c][j] != i for j in range(1, MAX_ITEMS+1)])))
 
 # - path -> b_path
-for c in COURIERS:
-    for j in range(max_nodes): # If node j is present in the path of courier c, set the corresponding item in b_path to True
+for c in Couriers:
+    for j in range(MAX_ITEMS): # If node j is present in the path of courier c, set the corresponding item in b_path to True
         optimizer.add(Implies(path[c][j] != n+1, b_path[c][path[c][j]] == True))
         # If node j is not present in the path of courier c, set the corresponding item in b_path to False
         optimizer.add(Implies(path[c][j] == n+1, b_path[c][path[c][j]] == False))
 
 # If you have more load size than me, then your load must be greater than mine
-for c1 in COURIERS:
-    for c2 in COURIERS:
+for c1 in Couriers:
+    for c2 in Couriers:
         if c2 > c1:
-            optimizer.add(If(load[c2] < load[c1], Sum([If(b_path[c1][j], size[j], 0) for j in NODES])
-                        <= Sum([If(b_path[c2][j], size[j], 0) for j in NODES]), True))
+            optimizer.add(If(load[c2] < load[c1], Sum([If(b_path[c1][j], size[j], 0) for j in Items])
+                        <= Sum([If(b_path[c2][j], size[j], 0) for j in Items]), True))
 
-# The items weight cannot exceed the load size (redundant? since channeling is there...)
-for c in COURIERS:
-    optimizer.add(Sum([If(b_path[c][j], size[j], 0) for j in NODES]) <= load[c])
-    optimizer.add(Sum([If(j < path_length[c]-1, size[path[c][j]], 0) for j in range(2, max_nodes)]) <= load[c])
+# The items weight cannot exceed the load size (NOTE: redundant? since channeling is there...)
+for c in Couriers:
+    optimizer.add(Sum([If(b_path[c][j], size[j], 0) for j in Items]) <= load[c])
+    optimizer.add(Sum([If(j < path_length[c]-1, size[path[c][j]], 0) for j in range(2, MAX_ITEMS)]) <= load[c])
             
 # Distance computation
-for c in COURIERS:
-    # Sum for distances between two non-zero nodes
+for c in Couriers:
+    # Sum for distances between two non-zero Items
     dist_expr = Sum([If(And(path[c][j] != 0, path[c][j+1] != 0), D_func(path[c][j]-1, path[c][j+1]-1), 0)
-                    for j in range(1, max_nodes)])
+                    for j in range(1, MAX_ITEMS)])
     
-    # Sum for distances when there's a zero node between two non-zero nodes
+    # Sum for distances when there's a zero node between two non-zero items
+    # FIXME: allowing zeros in the paths, we have to do this additional computation
     dist_expr += Sum([If(And(path[c][j] == 0, path[c][j-1] != 0, path[c][j+1] != 0), D_func(path[c][j-1]-1, path[c][j+1]-1), 0)
-                      for j in range(1, max_nodes)])
+                      for j in range(1, MAX_ITEMS)])
     
     optimizer.add(total_distance[c] == dist_expr)
 
-'''
-# No zeros in each path: IT DOES NOT WORK
-for c in COURIERS:
-    for i in range(2, max_nodes):
-        optimizer.add(Implies(i < path_length[c]-1, path[c][i] != 0))
-        optimizer.add(Implies(i < path_length[c]-1, path[c][i+1] != 0))
-'''
-
 # Symmetry breaking: two couriers with the same load size
-for c1 in COURIERS:
-    for c2 in COURIERS:
+for c1 in Couriers:
+    for c2 in Couriers:
         if c1 < c2:  # Ensure c1 < c2 to avoid redundant comparisons
             # Add symmetry-breaking constraint if load sizes are equal
-            sym_break_constraint = If(load[c1] == load[c2], lexleq([b_path[c1][j] for j in NODES], [b_path[c2][j] for j in NODES]), True)
+            sym_break_constraint = If(load[c1] == load[c2], lexleq([b_path[c1][j] for j in Items], [b_path[c2][j] for j in Items]), True)
             optimizer.add(sym_break_constraint)
 
 # OPTIMIZATION OBJECTIVE - Minimize the maximum distance traveled by any courier
 max_dist = Int('max_dist')
-optimizer.add([max_dist >= total_distance[c] for c in COURIERS])
+optimizer.add([max_dist >= total_distance[c] for c in Couriers])
 optimizer.minimize(max_dist)
 
 # CHECK SATISFIABILITY
 if optimizer.check() == sat:
     model = optimizer.model()
 
-    # Print the path for each courier
+    # Print the path for each courier (NOTE: without zeros)
     print("\nCouriers' paths")
-    for c in COURIERS:
+    paths = {}
+    for c in Couriers:
+        path_length_c = model.eval(path_length[c]).as_long()
+        path_values = []
+        for j in range(1, path_length_c + 1):
+            evaluated_value = model.evaluate(path[c][j]).as_long()
+            if evaluated_value != 0:
+                path_values.append(evaluated_value)
+        paths[c] = path_values
+        print(f'Courier {c}: {path_values}')
+
+    '''
+    # Print the path for each courier (NOTE: with zeros)
+    print("\nCouriers' paths")
+    for c in Couriers:
         path_values = [model.evaluate(path[c][j]) for j in range(1, model.eval(path_length[c]+1).as_long())]
         print(f'Courier {c}: {path_values}')
+    '''
     
     '''
     # Print the item assignments in b_path
-    for c in COURIERS:
-        item_assignments = [model.evaluate(b_path[c][j]) for j in NODES]
+    for c in Couriers:
+        item_assignments = [model.evaluate(b_path[c][j]) for j in Items]
         print(f'Courier {c} item assignments: {item_assignments}') 
     '''  
 
-    #print("path length:", [model.eval(path_length[c]) for c in COURIERS]) 
-
     print("\nTotal distances")
-    for c in COURIERS:
+    for c in Couriers:
         print(f"Courier {c}: {model.eval(total_distance[c])}")
     
     print(f"\nMax distance: {model.eval(max_dist)}")
@@ -205,6 +223,34 @@ if optimizer.check() == sat:
     # Calculate elapsed time
     elapsed_time = end_time - start_time
     print(f"\nElapsed time: {elapsed_time} seconds")
+
+    # Plot the paths using networkx
+    G = nx.Graph()
+    
+    # Add nodes
+    for i in range(1, n + 2):
+        G.add_node(i)
+    
+    # Add edges for each courier's path
+    colors = ['b', 'g', 'r', 'c', 'm', 'y', 'k']  # Add more colors if needed
+    for c in Couriers:
+        path_values = paths[c]
+        edges = [(path_values[i], path_values[i + 1]) for i in range(len(path_values) - 1)]
+        edges.append((path_values[-1], path_values[0]))  # To complete the loop
+        G.add_edges_from(edges, color=colors[c % len(colors)], weight=2)
+
+    # Get edges and colors
+    edges = G.edges()
+    edge_colors = [G[u][v]['color'] for u, v in edges]
+    edge_weights = [G[u][v]['weight'] for u, v in edges]
+
+    # Draw the graph
+    pos = nx.circular_layout(G)
+    edges = [(u, v) for u, v in edges if u != v]
+    nx.draw(G, pos, edgelist=edges, edge_color=edge_colors, width=edge_weights, with_labels=True, node_size=500, node_color='lightblue')
+
+    plt.title('Paths taken by Couriers')
+    plt.show()
 
 else:
     print("unsat")
