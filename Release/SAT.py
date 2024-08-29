6813c052f05e14ba6f2b0c791725928d2330f8f2
t-perniola <t.perniola1@studenti.uniba.it>
2024-08-29 17:09:40 +0200

    ,minor fixes

--- Release/SAT.py
+++ Release/SAT.py
@@ -20,39 +20,39 @@ def mcp(instance, timeout=3000000):
 
     # VARIABLES
     # - Represent that each courier c picks the item i at the time step t (add 1 for depot)
-    v = [[[Bool(f"x_{c}_{i}_{t}") for t in range(max_times + 1)] for i in range(n + 1)] for c in range(m)]
+    pickup_schedule = [[[Bool(f"x_{c}_{i}_{t}") for t in range(max_times + 1)] for i in range(n + 1)] for c in range(m)]
     
     # CONSTRAINTS
     # 1) Each courier c can carry at most l[c] kg
     for c in range(m):
-        weight_set = []
+        load_list = []
         for i in range(n):
             for t in range(1, max_times):
                 for _ in range(s[i]):
-                    weight_set.append(v[c][i][t])
-        solver.add(at_most_k_seq(weight_set, l[c], f"courier_{c}_load"))
+                    load_list.append(pickup_schedule[c][i][t])
+        solver.add(at_most_k_seq(load_list, l[c], f"courier_{c}_load"))
         
     # 2) Each courier c starts and ends at position i = n
     for c in range(m):
-        solver.add(v[c][n][0] == True)  # Start at position n at time 0
-        solver.add(v[c][n][max_times] == True)  # End at position n at max_times
+        solver.add(pickup_schedule[c][n][0] == True)  # Start at position n at time 0
+        solver.add(pickup_schedule[c][n][max_times] == True)  # End at position n at max_times
     
     # 3) Each courier must deliver at least one item
     for c in range(m):
-        solver.add(at_least_one_bw([v[c][i][t] for t in range(1, max_times) for i in range(n)]))
+        solver.add(at_least_one_bw([pickup_schedule[c][i][t] for t in range(1, max_times) for i in range(n)]))
     
     # 4) Each courier cannot pick the same item more than once
     for c in range(m):
         for i in range(n):
-            solver.add(at_most_one_bw([v[c][i][t] for t in range(1, max_times)], f"exactly_once_courier{c}"))
+            solver.add(at_most_one_bw([pickup_schedule[c][i][t] for t in range(1, max_times)], f"exactly_once_courier{c}"))
 
     # 5) Each item is taken exactly once among all couriers and across all timesteps
     for i in range(n):
-        solver.add(exactly_one_bw([v[c][i][t] for t in range(1, max_times) for c in range(m)], f"exactly_once_{i}"))
+        solver.add(exactly_one_bw([pickup_schedule[c][i][t] for t in range(1, max_times) for c in range(m)], f"exactly_once_{i}"))
     
     # 6) All items should be picked up
     for i in range(n):
-        solver.add(at_least_one_bw([v[c][i][t] for t in range(1, max_times) for c in range(m)]))
+        solver.add(at_least_one_bw([pickup_schedule[c][i][t] for t in range(1, max_times) for c in range(m)]))
 
     '''# 7) Symmetry breaking: two couriers have the same load size
     for c1, c2 in combinations(range(m), 2):
@@ -62,7 +62,7 @@ def mcp(instance, timeout=3000000):
                     solver.add(Or(Not(v[c1][i][t]), v[c2][i][t]))'''
     
     # return model and the updated 3D variable
-    return solver, v
+    return solver, pickup_schedule
 
 # Calculate distances relatively to the paths retrieved as solution
 def compute_distances(solution, instance):
@@ -74,12 +74,12 @@ def compute_distances(solution, instance):
         if not courier_solution:
             continue
         total_distance = 0
-        depot = n+1  # Starting node
+        depot = n  # Starting node
         for node in courier_solution:
             if node != depot:
-                total_distance += distances[depot-1][node-1]
+                total_distance += distances[depot][node]
                 depot = node
-        total_distance += distances[depot-1][n]  # Return to starting node
+        total_distance += distances[depot][n]  # Return to starting node
         distances_dict[i] = total_distance
 
     return distances_dict
@@ -95,67 +95,49 @@ def extract_solution(model, instance, v):
     max_times = instance["max_times"]
     solution = []
     for c in range(m):
-        courier_solution = []
-        for t in range(max_times+1):
-            for i in range(n+1):
+        courier_path = []
+        for t in range(max_times):
+            for i in range(n):
                 if model[v[c][i][t]]:
-                    courier_solution.append(i + 1)  # Store 1-based index for better readability
-        solution.append(courier_solution)
+                    courier_path.append(i)
+        solution.append(courier_path)
     return solution
 
-# Example function to compute additional info for the instance
+# Additional info for the instance
 def compute_additional_info(instance):
     n = instance["n"]
     m = instance["m"]
-    D = instance["D"]
 
     # Number of max_times steps
     max_times = (n // m) + 3
-
-    # Minimum load (min among all sizes)
-    min_load = min(instance["s"])
-
-    # Max load (max among all sizes)
-    max_load = max(instance["s"])
-
-    # Maximum distance
-    max_distance = np.sum(np.max(D, axis=1))
-
-    additional_info = {
-        "max_times": max_times,
-        "max_dist": max_distance,
-        "min_dist": 0,
-        "min_load": min_load,
-        "max_load": max_load,
-    }
-
+    additional_info = {"max_times": max_times,}
     return additional_info
 
-def write_results_to_json(instance_number, solution, distances_dict, max_dist, time_taken):
-    result = {
-        "instance_number": instance_number,
-        "time_taken": int(time_taken),
-        "max_distance": max_dist,
-        "courier_paths": {}
-    }
+def write_results_to_json(instance_number, solution, max_dist, time_taken):
+    TIME_LIMIT = 300
 
-    for courier, path in enumerate(solution):
-        result["courier_paths"][f"courier_{courier + 1}"] = {
-            "path": path,
-            "total_distance": distances_dict[courier]
-        }
+    # Prepare the paths list based on the solution
+    paths = []
+    for courier_path in solution:
+        path = [item for item in courier_path]
+        paths.append(path)
 
-    output_dir = 'res/SAT/'
-    os.makedirs(output_dir, exist_ok=True)
-    output_path = os.path.join(output_dir, f"{instance_number}.json")
-    
-    with open(output_path, 'w') as f:
-        json.dump(result, f, indent=4)
+    # Prepare JSON dictionary
+    json_dict = {}
+    json_dict['SAT'] = {}
+    json_dict['SAT']['time'] = int(time_taken)
+    json_dict['SAT']['optimal'] = True if (time_taken < TIME_LIMIT) else False
+    json_dict['SAT']['obj'] = max_dist if max_dist is not None else None
+    json_dict['SAT']['sol'] = paths
+
+    # Write JSON to file
+    with open(f'res/SAT/{str(int(instance_number))}.json', 'w') as outfile:
+        json.dump(json_dict, outfile)
 
 def main(instance_number):
     start_time = time.time()
     
-    # IMPORTING INSTANCE
+    # Importing specified instance
     try:
         file_path = os.path.join('Instances', f'inst{instance_number}.dat')
         instance = read_dat_file(file_path)
@@ -176,12 +158,12 @@ def main(instance_number):
         max_dist = compute_max_dist(distances_dict)
         time_taken = time.time() - start_time
         print(f"\nObj. function value, max dist: {max_dist}")
-        write_results_to_json(instance_number, solution, distances_dict, max_dist, time_taken)
+        write_results_to_json(instance_number, solution, max_dist, time_taken)
     else:
         print("unsat")
 
-# remove later
+# TODO remove later
 if __name__ == "__main__":
-    instance_number = "05"  # Example instance number
+    instance_number = "03"  # Example instance number
     os.chdir("Release")
     main(instance_number)
