028bab87a3d5fa10fc8d646473f0bd8f9ffd98f4
Armando Renzullo <115501474+ArmyUser@users.noreply.github.com>
2025-02-03 00:16:00 +0100

    Add files via upload

--- Release/SAT/SAT.py
+++ Release/SAT/SAT.py
@@ -1,169 +1,179 @@
+from z3 import BoolVal, Bool, Implies, And, Solver, sat, is_true
 import time
-from z3 import *
-import numpy as np
-import json
 import os
-#from itertools import combinations
-from encodings_utils import *
+import multiprocessing as mp
+from utils import *
 
-# Modelling MCP problem
-def mcp(instance, timeout=3000000):
-    m = instance["m"]  # Number of couriers
-    n = instance["n"]  # Number of items
-    l = instance["l"]  # Weights each courier can carry
-    s = instance["s"]  # Items' sizes
-    max_times = instance["max_times"]  # Time horizon: a courier can carry at most max_times items..
-    # .. and since at each timestep a courier can pick exactly one item, max_times refers to the max time steps possible
+def serialize_model(model, vars_list):
+    serialized = {}
+    for var in vars_list:
+        eval_var = model.eval(var, model_completion=True)
+        if str(eval_var.sort()) == "Bool":
+            serialized[str(var)] = is_true(eval_var)
+        else:
+            serialized[str(var)] = eval_var.as_long()
+    return serialized
 
+def model(m, n, l, s, D, lb, ub, time_limit, init_flag, result_queue, search_method='binary', symm=False):
+    start_time = time.time()
     solver = Solver()
-    solver.set("timeout", timeout)
     solver.set("random_seed", 42)
 
-    # VARIABLES
-    # - Represent that each courier c picks the item i at the time step t (add 1 for depot)
-    pickup_schedule = [[[Bool(f"x_{c}_{i}_{t}") for t in range(max_times + 1)] for i in range(n + 1)] for c in range(m)]
+    depot = n  
+    max_load = sum(s)
+    s_max = max(s)
+    l_max = max(l)
+    path = [[[Bool(f'p_{courier}_{package}_{step}') for step in range(n + 2)]
+             for package in range(n + 1)] for courier in range(m)]
+    cw = [[Bool(f'w_{courier}_{package}') for package in range(n)]
+                       for courier in range(m)]
+    cl = [[Bool(f'cl_{courier}_{bit}') for bit in range(number_of_bits(max_load))]
+                     for courier in range(m)]
+    cd = [[Bool(f'cdt_{courier}_{bit}') for bit in range(number_of_bits(ub))]
+                  for courier in range(m)]
+    max_d = [Bool(f'max_d_{bit}') for bit in range(number_of_bits(ub))]
+    c_step_d = [[[Bool(f"cpt_{courier}_{step}_{bit}") for bit in range(number_of_bits(ub))]
+                   for step in range(n+1)] for courier in range(m)]
+
+    s_b = [int_to_bool_list(s_value, number_of_bits(s_max)) for s_value in s]
+    l_b = [int_to_bool_list(l_value, number_of_bits(l_max)) for l_value in l]
+    D_b = [[int_to_bool_list(D[i][j], number_of_bits(ub)) for j in range(n+1)]
+           for i in range(n+1)]
     
-    # CONSTRAINTS
-    # 1) Each courier c can carry at most l[c] kg
-    for c in range(m):
-        load_list = []
-        for i in range(n):
-            for t in range(1, max_times):
-                for _ in range(s[i]):
-                    load_list.append(pickup_schedule[c][i][t])
-        solver.add(at_most_k_seq(load_list, l[c], f"courier_{c}_load"))
+    for courier in range(m):
+        for step in range(n + 2):
+            solver.add(exactly_one([path[courier][package][step] for package in range(n + 1)]))
+    
+    for package in range(n):
+        solver.add(exactly_one([path[courier][package][step] for courier in range(m)
+                                   for step in range(n + 2)]))
+
+    for courier in range(m):
+        solver.add(path[courier][depot][0] == True)
+        solver.add(path[courier][depot][n + 1] == True)
+        solver.add(conditional_binary_sum(s_b, cw[courier], cl[courier], f"courier_load_{courier}"))
+        solver.add(geq_bool_lists(l_b[courier], cl[courier]))
         
-    # 2) Each courier c starts and ends at position i = n
-    for c in range(m):
-        solver.add(pickup_schedule[c][n][0] == True)  # Start at position n at time 0
-        solver.add(pickup_schedule[c][n][max_times] == True)  # End at position n at max_times
+        for step in range(n + 2):
+            for package in range(n):
+                solver.add(Implies(path[courier][package][step], cw[courier][package]))
     
-    # 3) Each courier must deliver at least one item
-    for c in range(m):
-        solver.add(at_least_one_bw([pickup_schedule[c][i][t] for t in range(1, max_times) for i in range(n)]))
+    if symm:
+        for c1 in range(m):
+            for c2 in range(m):
+                if c1 < c2 and l[c1] == l[c2]:
+                    solver.add(lexicographical_less(
+                        [cw[c1][p] for p in range(n)],
+                        [cw[c2][p] for p in range(n)]
+                    ))
+
+    for courier in range(m):
+        for step in range(n + 1):
+            for package_start in range(n + 1):
+                for package_end in range(n + 1):
+                    solver.add(Implies(
+                        And(path[courier][package_start][step],
+                            path[courier][package_end][step+1]),
+                        equal_bool_lists(D_b[package_start][package_end],
+                                         c_step_d[courier][step])
+                    ))
+        solver.add(conditional_binary_sum(c_step_d[courier],
+                                [BoolVal(True) for _ in range(n+1)],
+                                cd[courier],
+                                f"def_courier_dist_{courier}"))
     
-    # 4) Each courier cannot pick the same item more than once
-    for c in range(m):
-        for i in range(n):
-            solver.add(at_most_one_bw([pickup_schedule[c][i][t] for t in range(1, max_times)], f"exactly_once_courier{c}"))
-
-    # 5) Each item is taken exactly once among all couriers and across all timesteps
-    for i in range(n):
-        solver.add(exactly_one_bw([pickup_schedule[c][i][t] for t in range(1, max_times) for c in range(m)], f"exactly_once_{i}"))
+    solver.add(max_bool_variable(cd, max_d))
+    solver.add(geq_bool_lists(max_d, int_to_bool_list(lb, number_of_bits(lb))))
     
-    # 6) All items should be picked up
-    for i in range(n):
-        solver.add(at_least_one_bw([pickup_schedule[c][i][t] for t in range(1, max_times) for c in range(m)]))
-
-    '''# 7) Symmetry breaking: two couriers have the same load size
-    for c1, c2 in combinations(range(m), 2):
-        if l[c1] == l[c2]:
-            for t in range(1, max_times):
-                for i in range(n):
-                    solver.add(Or(Not(v[c1][i][t]), v[c2][i][t]))'''
+    init_flag.value = True
+    best_model = None
     
-    # return model and the updated 3D variable
-    return solver, pickup_schedule
+    if search_method == 'branch_and_bound':
+        solver.push()
+        while True:
+            remaining_time = max(1, (time_limit - int((time.time() - start_time) * 1000)))
+            solver.set("timeout", remaining_time)
+            solver.push()
+            solver.add(geq_bool_lists(int_to_bool_list(ub, number_of_bits(ub)), max_d))
+            if solver.check() == sat:
+                best_model = solver.model()
+                ub = bool_list_to_int([best_model[b] for b in max_d]) - 1  
+            else:
+                solver.pop()
+                break  
+            solver.pop()
+        solver.pop()
+    elif search_method == 'binary':
+        while ub - lb > 1:
+            remaining_time = max(1, (time_limit - int((time.time() - start_time) * 1000)))
+            solver.set("timeout", remaining_time)
+            mid = (ub + lb) // 2
+            solver.push()
+            solver.add(geq_bool_lists(int_to_bool_list(mid, number_of_bits(ub)), max_d))
+            if solver.check() == sat:
+                best_model = solver.model()
+                ub = bool_list_to_int([best_model[b] for b in max_d])
+            else:
+                lb = mid  
+            solver.pop()
+    
+    if best_model is None:
+        result_queue.put((time.time() - start_time, None, None, search_method))
+        return
+    
+    obj_value = bool_list_to_int([best_model[b] for b in max_d])
+    flat_vars = [var for courier in path for package in courier for var in package]
+    serialized_solution = serialize_model(best_model, flat_vars)
+    result_queue.put((time.time() - start_time, obj_value, serialized_solution, search_method))
 
-# Calculate distances relatively to the paths retrieved as solution
-def compute_distances(solution, instance):
-    distances = instance["D"]
-    n = instance["n"]
-    distances_dict = {}
-
-    for i, courier_solution in enumerate(solution):
-        if not courier_solution:
-            continue
-        total_distance = 0
-        depot = n  # Starting node
-        for node in courier_solution:
-            if node != depot:
-                total_distance += distances[depot][node-1]
-                depot = node-1
-        total_distance += distances[depot][n]  # Return to starting node
-        distances_dict[i] = total_distance
-
-    return distances_dict
-
-# Objective function
-def compute_max_dist(distances_dict):
-    return max(distances_dict.values())
-
-# Retrieve the solution
-def extract_solution(model, instance, v):
-    m = instance["m"]  # Number of couriers
-    n = instance["n"]  # Number of packages
-    max_times = instance["max_times"]
-    solution = []
-    for c in range(m):
-        courier_path = []
-        for t in range(max_times):
-            for i in range(n):
-                if model[v[c][i][t]]:
-                    courier_path.append(i+1)
-        solution.append(courier_path)
-    return solution
-
-# Additional info for the instance
-def compute_additional_info(instance):
-    n = instance["n"]
-    m = instance["m"]
-
-    # Number of max_times steps
-    max_times = (n // m) + 3
-    additional_info = {"max_times": max_times}
-    return additional_info
-
-def write_results_to_json(instance_number, solution, max_dist, time_taken):
-    TIME_LIMIT = 300
-    opt_values = {"01": 14, "02": 226, "03": 12, "04": 220, "05": 206, "06": 322, "07": 167,
-                  "08": 186, "09": 436, "10": 244}
-
-    # Prepare the paths list based on the solution
-    paths = []
-    for courier_path in solution:
-        path = [item for item in courier_path]
-        paths.append(path)
-
-    # Prepare JSON dictionary
-    json_dict = {}
-    json_dict['SAT'] = {}
-    json_dict['SAT']['time'] = int(time_taken)
-    json_dict['SAT']['optimal'] = True if (time_taken < TIME_LIMIT and opt_values[instance_number] == max_dist) else False
-    json_dict['SAT']['obj'] = max_dist if max_dist is not None else None
-    json_dict['SAT']['sol'] = paths
-
-    # Write JSON to file
-    with open(f'res/SAT/{str(int(instance_number))}.json', 'w') as outfile:
-        json.dump(json_dict, outfile)
-
-def SAT(instance_number):
+def solve_problem(m, n, l, s, D, lb, ub, time_limit, search_method="binary", symm=False):
+    result_queue = mp.Queue()
+    init_flag = mp.Value('b', False)  
+    process = mp.Process(target=model, args=(m, n, l, s, D, lb, ub, time_limit, init_flag, result_queue, search_method, symm))
+    process.start()
+    
     start_time = time.time()
-    
-    # Importing specified instance
-    try:
-        file_path = os.path.join('Instances', f'inst{instance_number}.dat')
-        instance = read_dat_file(file_path)
-    except Exception as e:
-        print(f"Error reading the instance file: {e}")
-        return None
-    
-    # Add useful info to instance
-    instance.update(compute_additional_info(instance))
-
-    # Running the model
-    solver, v = mcp(instance=instance)
-    
-    if solver.check() == sat:
-        model = solver.model()
-        solution = extract_solution(model, instance, v)
-        distances_dict = compute_distances(solution, instance)
-        max_dist = compute_max_dist(distances_dict)
-        time_taken = time.time() - start_time
-        
-        print(f"\nObj. function value, max dist: {max_dist}")
-
-        write_results_to_json(instance_number, solution, max_dist, time_taken)
+    while time.time() - start_time < time_limit / 1000:
+        time.sleep(1)
+        if init_flag.value:
+            break
     else:
-        print("unsat")
+        process.terminate()
+        process.join()
+        return time.time() - start_time, None, None, search_method
+    
+    process.join() 
+    
+    try:
+        result = result_queue.get_nowait()
+    except Exception as e:
+        print(f"Error: {e}")
+        return time.time() - start_time, None, None, search_method
+    
+    return result
 
+if __name__ == "__main__":
+    mp.set_start_method("spawn")
+    instance_num = "01"
+    time_limit = 300000  
+    file_path = os.path.join('Instances', f'inst{instance_num}.dat')
+    instance = read_data_file(file_path)
+    m = instance["m"]
+    n = instance["n"]
+    l = instance["l"]
+    s = instance["s"]
+    D = instance["D"]
+    lb = instance["lb"]
+    ub = instance["ub"]
+
+    time_taken, obj_value, solution, search_method = solve_problem(
+        m, n, l, s, D, lb, ub, time_limit,
+        search_method='branch_and_bound',
+        symm=True
+    )
+    if solution:
+        depot = n
+        solution = refine_solution(time_taken, obj_value, solution, search_method, depot, time_limit)
+        print('Solution:', solution)
+    else:
+        print(f'"{search_method}": {{\n"obj": null,\n"sol": null,\n"optimal": false,\n"time": {int(time_taken)}\n}}')
